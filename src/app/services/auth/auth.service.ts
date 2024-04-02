3d8516d41c85c4a6791bc98feac1d1445b15b3dd
moustaphas.dieme <ndieme16@hotmail.com>
2024-04-03 00:30:08 +0200

    use Input/ Output/ EventEmiter

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from '../../model/user.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,41 @@ import { User } from '../../model/user.model';
 export class AuthService {
   users: User[] = [{"email":"admin@em.com","password":"123","roles":['ADMIN']},
   {"email":"nadhem@em.com","password":"123","roles":['USER']} ];
-  public loggedUser!:string;
 
-  constructor() { }
+  public loggedUser?: string;
+  public isloggedIn: Boolean = false;
+  public roles?: string[];
+  constructor(private router: Router) { }
+
+  logout() {
+    this.isloggedIn= false;
+    this.loggedUser = undefined!;
+    this.roles = undefined!;
+    localStorage.removeItem('loggedUser');
+    localStorage.setItem('isloggedIn',String(this.isloggedIn));
+    this.router.navigate(['/login']);
+  }
+
+  SignIn(user :User):Boolean{
+    let validUser: Boolean = false;
+    this.users.forEach((curUser) => {
+      if(user.email == curUser.email && user.password==curUser.password) {
+        validUser = true;
+        this.loggedUser = curUser.email;
+        this.isloggedIn = true;
+
+        this.roles = curUser.roles;
+        localStorage.setItem('loggedUser', this.loggedUser!);
+        localStorage.setItem('isloggedIn', String(this.isloggedIn));
+      }
+    });
+    return validUser;
+  }
+
+  isAdmin():Boolean{
+    if (!this.roles) //this.roles== undefiened
+      return false;
+
+    return (this.roles.indexOf('ADMIN') >-1);
+  }
 }
